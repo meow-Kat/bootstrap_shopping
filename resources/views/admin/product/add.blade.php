35d12c1e126f94fb82e703ed05fcd366dad4d724
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-09-06 23:01:33 +0800

    meow

--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -14,11 +14,10 @@
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
-                <img style="width: 450px; height: 300px;" id="preview_progressbarTW_img"
-                    src="https://dummyimage.com/600x400">
-                <input class="py-3" type="file" onchange="readURL(this)" targetID="preview_progressbarTW_img"
-                    {{-- 接受圖片型式 --}}
-                accept="image/*" name="product_photo">
+                    <img style="width: 450px; height: 300px;" id="preview_progressbarTW_img"
+                        src="https://dummyimage.com/600x400">
+                    <input class="py-3" type="file" onchange="readURL(this)" targetID="preview_progressbarTW_img"
+                        {{-- 接受圖片型式 --}} accept="image/*" name="product_photo">
                 </div>
 
                 <div class="form-group">
@@ -29,19 +28,31 @@
 
                 <div class="form-group">
                     <label class="pr-3" for="product_size">尺寸</label>
-                    <label><input type="checkbox" class="mx-2" name="product_size[]" value="XS">XS</label>
+                    {{-- <label><input type="checkbox" class="mx-2" name="product_size[]" value="XS">XS</label>
                     <label><input type="checkbox" class="mx-2" name="product_size[]" value="S">S</label>
                     <label><input type="checkbox" class="mx-2" name="product_size[]" value="M">M</label>
                     <label><input type="checkbox" class="mx-2" name="product_size[]" value="L">L</label>
                     <label><input type="checkbox" class="mx-2" name="product_size[]" value="XL">XL</label>
-                    <label><input type="checkbox" class="mx-2" name="product_size[]" value="XXL">XXL</label>
+                    <label><input type="checkbox" class="mx-2" name="product_size[]" value="XXL">XXL</label> --}}
+                    @foreach ($size as $key => $item)
+                        <p class="mb-0 mr-2" style="line-height: 25px;">{{ $item }}</p>
+                        <input id="size" type="checkbox" class="form-control mr-3" style="width: 25px; height: 25px;"
+                            name="size[]" value="{{ $item }}" autocomplete="size" autofocus>
+                    @endforeach
                 </div>
 
                 <div class="form-group color-group">
                     <label class="pr-3" for="product_color">顏色</label>
-                    <input type="color" class="mx-2" id="product_color[]" name="product_color">
+                    {{-- <input type="color" class="mx-2" id="product_color[]" name="product_color">
                     <span class="add-color-space"></span>
-                    <button type="button" class="btn btn-sm btn-secondary">增加顏色</button>
+                    <button type="button" class="btn btn-sm btn-secondary">增加顏色</button> --}}
+                    <input class="btn btn-outline-primary btn-sm h-75 addColor" name="addBtn" type="button" value="+">
+                    <div class="row m-0 d-flex align-items-center mb-3">
+                        <input id="color" type="color" class="form-control w-25 mr-3" name="color[]" autocomplete="color"
+                            autofocus>
+                        <input class="btn btn-outline-danger btn-sm h-75 addColor" name="addBtn" type="button" value='-'
+                            onclick='$(this).parent().remove();'>
+                    </div>
                 </div>
             </div>
             <div class="col-6">
@@ -86,11 +97,23 @@
             }
         }
 
-        let addColor = document.querySelector('.btn-secondary')
-        addColor.onclick = function () {
-            var color = document.querySelector('.add-color-space')
-            color.innerHTML = `<input type="color" class="mx-2" id="color" name="color[]">
-            <span class="add-color-space"></span>`
+        // let addColor = document.querySelector('.btn-secondary')
+        // addColor.onclick = function() {
+        //     var color = document.querySelector('.add-color-space')
+        //     color.innerHTML = `<input type="color" class="mx-2" id="color" name="color[]">
+        //     <span class="add-color-space"></span>`
+        
+        // 增加顏色
+        var addColor = document.querySelector('.addColor')
+        // var i = 1;
+        addColor.addEventListener('click', function() {
+            // i++;
+            var newColor = document.createElement('div');
+            newColor.className = 'row m-0 d-flex align-items-center';
+            newColor.innerHTML =
+                "<input type='color' name='color[]' class='form-control w-25 mb-3 mr-3'><input type='button' name='removeBtn' class='btn btn-sm btn-outline-danger mb-3' value='-' onclick='$(this).parent().remove();'>"
+            document.getElementById('newColor').appendChild(newColor);
+        });
         }
     </script>
 @endsection
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.app')
 
-@section('title','產品管理')
+@section('title', '產品管理')
 
-@section('h1_title','產品管理')
+@section('h1_title', '產品管理')
 
 {{-- 找datatable CSS --}}
 @section('css')
@@ -10,68 +10,74 @@
 @endsection
 
 @section('content')
-<div class="py-3">
-    <a href="{{ asset('/admin/product/add') }}" type="button" class="btn btn-info">新增產品</a>
-</div>
-<table id="my-datatable" class="display" style="width:100%">
-    <thead>
-        <tr>
-            <th>分類</th>
-            <th>照片</th>
-            <th>名稱</th>
-            <th>價格</th>
-            <th>至頂</th>
-            <th>尺寸</th>
-            <th>操作</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($record as $item)
-        <tr>
-            <td>{{ $item->type->type_name }}</td>
-            <td><img src="{{ $item->product_photo }}" alt="" style="width: 300px; height: 200px;"></td>
-            <td>{{ $item->product_name }}</td>
-            <td>{{ $item->product_price }}</td>
-            <?php
-                $size = json_decode($item->product_size);
-            ?>
-            <td>
-                @foreach ($size as $sizes)
-                    {{$sizes}},
-                @endforeach
-            </td>
-            <td>
-                <a href="{{ asset('/admin/product/edit') }}/{{ $item->id }}" type="button" class="btn btn-primary btn-sm d-flex justify-content-center">Edit</a>
-                <form action="{{ asset('/admin/product/delete') }}/{{  $item->id }} " method="POST">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-danger btn-sm mt-2 =">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
+    <div class="py-3">
+        <a href="{{ asset('/admin/product/add') }}" type="button" class="btn btn-info">新增產品</a>
+    </div>
+    <table id="my-datatable" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>分類</th>
+                <th>照片</th>
+                <th>名稱</th>
+                <th>價格</th>
+                <th>至頂</th>
+                <th>尺寸</th>
+                <th>顏色</th>
+                <th>操作</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($record as $item)
+                <tr>
+                    <td>{{ $item->type->type_name }}</td>
+                    <td><img src="{{ $item->product_photo }}" alt="" style="width: 300px; height: 200px;"></td>
+                    <td>{{ $item->product_name }}</td>
+                    <td>{{ $item->product_price }}</td>
+                    <td>
+                        @foreach ($size as $sizes)
+                            {{ $sizes }},
+                        @endforeach
+                    </td>
+                    <td>
+                        @foreach ($item->color as $color)
+                            <div class="showColor" style="background-color: {{ $color }}"></div>
+                        @endforeach
+                    </td>
+                    </td>
+                    <td>
+                        <a href="{{ asset('/admin/product/edit') }}/{{ $item->id }}" type="button"
+                            class="btn btn-primary btn-sm d-flex justify-content-center">Edit</a>
+                        <form action="{{ asset('/admin/product/delete') }}/{{ $item->id }} " method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger btn-sm mt-2 =">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
 
-    </tbody>
-    <tfoot>
-        <tr>
-            <th>分類</th>
-            <th>照片</th>
-            <th>名稱</th>
-            <th>價格</th>
-            <th>至頂</th>
-            <th>尺寸</th>
-            <th>操作</th>
-        </tr>
-    </tfoot>
-</table>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>分類</th>
+                <th>照片</th>
+                <th>名稱</th>
+                <th>價格</th>
+                <th>至頂</th>
+                <th>尺寸</th>
+                <th>顏色</th>
+                <th>操作</th>
+            </tr>
+        </tfoot>
+    </table>
 @endsection
 
 {{-- 找datatable JS --}}
 @section('js')
-<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#my-datatable').DataTable()
-    } )
-</script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#my-datatable').DataTable()
+        })
+    </script>
 @endsection
