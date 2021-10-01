868dca403415070eeefa0258b6fc67eab63e4876
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-10-01 10:37:38 +0800

    update

--- resources/views/front/login.blade.php
+++ resources/views/front/login.blade.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.template')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>log in</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+@section('title', 'login')
+
+@section('css')
     <link rel="stylesheet" href="./css/login.css">
-</head>
+    <style>
+        nav{
+            display: none;
+        }
+        footer{
+            display: none;
+        }
+    </style>
+@endsection
 
-<body>
 
+
+@section('main')
     <div class="row no-gutters">
         <div class="col">
             <div class="back-img position-absolute"></div>
@@ -43,18 +46,30 @@
                                     <i class="fab fa-linkedin-in link"></i>
                                 </div>
                                 <div class="text-center"><span>or use email your account</span></div>
-                                <form class="py-3">
+                                <form class="py-3" method="POST" action="{{ route('login') }}">
+                                    @csrf
                                     <div class="form-group">
-                                        <input type="email" class="form-control" id="exampleInputEmail1"
-                                            aria-describedby="emailHelp" placeholder="Enter email">
+                                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" aria-describedby="emailHelp" placeholder="請輸入 Email 帳號" value="{{ old('email') }}" required autofocus>
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" id="exampleInputPassword1"
-                                            placeholder="Password">
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="請輸入密碼" required>
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
-                                    <div class="text-right"><span>Forgot your password?</span></div>
+
+                                    <a class="text-right" href="{{ route('password.request') }}"><span>{{ __('Forgot Your Password?') }}</span></a>
                                 </form>
-                                <button type="button" class="btn btn-primary btn-lg w-100 my-botton">SING IN</button>
+                                <button type="button" class="btn btn-primary btn-lg w-100 my-botton">{{ __('Login') }}</button>
 
                                 <div class="left-bottom-1 text-center my-5 d-lg-none">
                                     <i class="fab fa-twitter font-icon"></i>
@@ -68,24 +83,13 @@
             </div>
         </div>
     </div>
+@endsection
 
 
 
 
 
 
+@section('js')
 
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+@endsection
